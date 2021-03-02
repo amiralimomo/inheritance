825e382000582b7c0a2ebfaa9cbf7662937026ee
AmirAli Mousa <amiralimomo77@gmail.com>
2021-03-02 19:34:09 +0330

    object constractor

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-s
+let User = function (name, balance) {
+    this.balance = balance
+    this.name = name
+    this.getrole = function () {
+        let name
+        if(this.balance>1000){
+            name='gold'
+        }else if(this.balance>500){
+            name='silver'
+        }
+        else{
+            name='normal'
+        }
+        return name
+    }
+}
+let user1=new User('amirali',7800)
+console.log(user1.getrole())
